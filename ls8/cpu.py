82e7d4b68bc2f17924313f88ac122f79c654a205
Richard Scott Prins Jr <richardsprins@gmail.com>
2020-01-29 23:08:16 -0500

    day 2 mvp done

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,15 +5,29 @@ import sys
 class CPU:
     """Main CPU class."""
 
+    instructions = {
+        0b10000010: 'LDI',
+        0b01000111: 'PRN',
+        0b00000001: 'HLT',
+        0b10100010: 'MUL'
+    }
+
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+        self.running = True
+        self.dispatch = {
+            'LDI': self.LDI,
+            'PRN': self.PRN,
+            'HLT':self.HLT,
+            'MUL': self.MUL
+        }
+
 
     def load(self):
         """Load a program into memory."""
-
         address = 0
 
         # For now, we've just hardcoded a program:
@@ -25,8 +39,9 @@ class CPU:
                     continue
                 value = int(data, 2)
 
-                
-
+                self.ram_write(address, value)
+                address += 1
+                # print(value)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -64,8 +79,44 @@ class CPU:
     def ram_write(self, address, value):
         self.ram[address] = value
 
+    def LDI(self):
+        self.pc += 1
+        op_1 = self.ram_read(self.pc)
+        self.pc += 1
+        op_2 = self.ram_read(self.pc)
+        self.reg[op_1] = op_2
+
+
+    def PRN(self):
+        self.pc += 1
+        op_1 = self.ram_read(self.pc)
+        value = self.reg[op_1]
+        print(value)
+        
+
+    def HLT(self):
+        self.running = False
+
+    def MUL(self):
+        self.pc += 1
+        op_1 = self.ram_read(self.pc)
+        self.pc += 1
+        op_2 = self.ram_read(self.pc)
+        math = self.reg[op_1] * self.reg[op_2]
+        self.reg[op_1] = math
+
     def run(self):
         """Run the CPU."""
-        running = True
-        while running is True:
+        
+        # print(self.ram)
+        while self.running:
             instruction_register = self.ram[self.pc]
+
+            # print(instruction_register)
+            if instruction_register in self.instructions:
+                command = self.instructions[instruction_register]
+                self.dispatch[command]()
+            
+            self.pc += 1
+
+
